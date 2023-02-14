239254aaa22f9c7f6977e32581d47fb8cbda9f7c
Maxwell1455 <96149379+Maxwell1455@users.noreply.github.com>
2023-02-14 13:26:22 -0600

    Database setup (#15)
    * Create db.js
    
    * Write schema
    
    * Squashed commit of the following:
    
    commit 52e8600cf5a746b57ed7f89b2c7097be94d2849a
    Merge: 3e89f77 e3de6c2
    Author: Blackkirby72 <47089182+Blackkirby72@users.noreply.github.com>
    Date:   Tue Feb 14 12:57:23 2023 -0600
    
        Merge pull request #14 from PistolRcks/bootstrap-fix
    
        Bootstrap fix
    
    commit e3de6c201af43cc48109b87d8be9d17383d3a324
    Author: Blackkirby72 <cczeck72@gmail.com>
    Date:   Tue Feb 14 12:55:35 2023 -0600
    
        bootstrap fix
    
    commit 3e89f776d46c482f14bfa37d300a13ab6601dfe8
    Merge: 5d684f8 6753056
    Author: Ella Prew <60476527+prewe5294@users.noreply.github.com>
    Date:   Tue Feb 14 12:29:52 2023 -0600
    
        Merge pull request #12 from PistolRcks/landing-page-ui
    
        Landing page/Puzzle Selection Page UI
    
    commit 6753056df2f9fb19221d6a589333c0536afde83f
    Author: Ella Prew <prewe5294@my.uwstout.edu>
    Date:   Tue Feb 14 11:01:31 2023 -0600
    
        Mock up of landing page made, with log in modal that brings you to the puzzle selection page, which i also made a mock up for
    
        will make these prettier in the future
    
    commit 396a9bd8ba55c5dc152387b2427f4eb6a78d6c21
    Merge: f3280ab 5d684f8
    Author: Ella Prew <prewe5294@my.uwstout.edu>
    Date:   Fri Feb 10 11:20:11 2023 -0600
    
        Merge branch 'master' into landing-page-ui
    
    commit 5d684f8b647277aa114f4ea5a69899bcef6fb0c6
    Merge: 8d2f592 007257d
    Author: Ella Prew <60476527+prewe5294@users.noreply.github.com>
    Date:   Fri Feb 10 11:18:59 2023 -0600
    
        Merge pull request #11 from PistolRcks/css-hotfix
    
        CSS stylesheet hotfix
    
    commit f3280ab17dfd10fa157e3c67374ecfce8fedbb91
    Author: Ella Prew <prewe5294@my.uwstout.edu>
    Date:   Fri Feb 10 11:17:42 2023 -0600
    
        updates jsons, landing page shows up, when log in isnt involved/used
    
    commit 007257defcb1284402f3d14cde8e2f7c2217baec
    Author: Blackkirby72 <47089182+Blackkirby72@users.noreply.github.com>
    Date:   Thu Feb 9 19:13:50 2023 -0600
    
        css hotfix
    
    commit e427bfaafde1773ccd2c56b911a4b1464ecf206a
    Author: Ella Prew <prewe5294@my.uwstout.edu>
    Date:   Thu Feb 9 15:20:55 2023 -0600
    
        attempt at creating landing page

---
+++ server/db.js
@@ -0,0 +1,38 @@
+var sqlite3 = require('sqlite3');
+
+var db = new sqlite3.Database('puzzle.db');
+
+db.serialize(function() {
+  // create the database schema for the todos app
+  db.run("CREATE TABLE IF NOT EXISTS User ( \
+    user_id INTEGER PRIMARY KEY IDENTITY, \
+    username TEXT UNIQUE, \
+    hashed_password BLOB, \
+    salt BLOB \
+  )");
+  
+  db.run("CREATE TABLE IF NOT EXISTS Puzzle ( \
+    puzzle_id INTEGER PRIMARY KEY IDENTITY, \
+    title TEXT NOT NULL, \
+    description TEXT \
+  )");
+
+  db.run("CREATE TABLE IF NOT EXISTS User Puzzle ( \
+    user_id INTEGER, \
+    puzzle_id INTEGER, \
+    time	INTEGER, \
+    progress	NUMERIC NOT NULL COLLATE BINARY, \
+    FOREIGN KEY(user_id) REFERENCES User(user_id), \
+    FOREIGN KEY(puzzle_id) REFERENCES Puzzle(puzzle_id) \
+  )");
+  
+  // create an initial user (username: alice, password: letmein)
+  var salt = crypto.randomBytes(16);
+  db.run('INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
+    'alice',
+    crypto.pbkdf2Sync('letmein', salt, 310000, 32, 'sha256'),
+    salt
+  ]);
+});
+
+module.exports = db;
