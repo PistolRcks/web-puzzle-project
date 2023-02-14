57405402bc32aff36a31a0bc8c51a5d18aa1a139
PistolRcks <17933691+PistolRcks@users.noreply.github.com>
2023-02-14 15:20:34 -0600

    Finish database integration
    Error messages are also handled.

--- server/api/signup.js
+++ server/api/signup.js
@@ -15,33 +15,35 @@ var db = require('../db');
  */
 function signup(req, res, next) {
     if (!req.body.password || !req.body.username) {
-      // TODO: eventually, this should be replaced with more sophisticated
-      // error handling
-      res.status(400).send("Username or password not set!")
+      res.status(400).send("Error: Username or password not set!");
+      return next("Error: Username or password not set!");
     }
 
     var salt = Crypto.randomBytes(16);      // salt is required for hashing; it makes 
                                             // otherwise identical hashes different
     Crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-      if (err) { return next(err); }
-      res.send([req.body.username, hashedPassword.toString('utf-8'), salt]);
+      if (err) {
+        res.status(500).send(err); 
+        return next(err); 
+      }
       // insert new user into database
-      db.run('INSERT INTO users (username, hashed_pass, salt) VALUES (?, ?, ?)', [
+      db.run('INSERT INTO User (username, hashed_password, salt) VALUES (?, ?, ?)', [
         req.body.username,
         hashedPassword,
         salt
       ], function(err) {
-        if (err) { return next(err); }
+        if (err) {
+          // TODO: Send more descriptive errors
+          res.status(500).send(err) 
+          return next(err); 
+        }
         var user = {
           id: this.lastID,
           username: req.body.username
         };
-        // FIXME: I have no clue what this might be or if it would work
-        // It's not in the express documentation...
-        req.login(user, function(err) {
-          if (err) { return next(err); }
-          res.redirect('/');    // TODO: eventually this should lead to the puzzle select page
-        });
+        // TODO: After correctly signing up, log the user in
+
+        res.status(200).send(`Successfully signed user ${user.username} up!`)
       });
     });
   };
