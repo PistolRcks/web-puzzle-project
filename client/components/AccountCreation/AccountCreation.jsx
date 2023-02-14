1aaff928d5e7b08422a9af8848fcaeb0285e86cb
kreklb <98837415+kreklb@users.noreply.github.com>
2023-02-14 12:17:16 -0600

    Account Creation

--- client/components/AccountCreation/AccountCreation.jsx
+++ client/components/AccountCreation/AccountCreation.jsx
@@ -1,49 +1,60 @@
 import React, {Component} from 'react';
-import './AccountCreation.css'
+import "./AccountCreation.css"
 
-function handleSubmit(event) {
-    console.log('hi')
-}
+const initialFormData = Object.freeze({
+    username: "",
+    password: "",
+    confirmPassword: ""
+});
 
-export default class AccountCreation extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+export default function AccountCreation(props) {
+    const [formData, updateFormData] = React.useState(initialFormData);
+
+    const handleChange = (e) => {
+        updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value.trim()
+        });
     }
-    handleChange(event) {
-        this.setState({value: event.target.value});
+    const handleChangePassword = (e) => {
+        updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value.trim()
+        });
+        console.log(e.target.value.trim());
     }
-    handleSubmit(event) {
-        alert("THIS IS YOUR PASSWORD: " + this.state.value);
-        event.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formData)
+        if(formData.password !== formData.confirmPassword) {
+            alert("YOUR PASSWORDS DO NOT MATCH")
+        }
+        
     }
-    render() {
-        return (
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Username:
-                        <br/>
-                        <input type="text" name="username" />
-                    </label>
-                    <label>
-                        <br/>
-                        Password:
-                        <br/>
-                        <input type="text" name="password" value={this.state.value} onChange={this.handleChange}/>
-                    </label>
-                    <label>
-                        <br/>
-                        Confirm Password:
-                        <br/>
-                        <input type="text" name="confirmPassword" />
-                    </label>
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username:
                     <br/>
-                    <button type="submit">Create</button>
-                </form>
-            </div>
-        );
-    }
+                    <input onChange={handleChange} name="username" />
+                </label>
+                <label>
+                    <br/>
+                    Password:
+                    <br/>
+                    <input onChange={handleChangePassword} name="password" />
+                </label>
+                <label>
+                    <br/>
+                    Confirm Password:
+                    <br/>
+                    <input onChange={handleChange} name="confirmPassword" />
+                </label>
+                <br/>
+            </form>
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    );
 }
