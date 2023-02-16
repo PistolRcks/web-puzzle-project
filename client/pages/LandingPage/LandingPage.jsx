a1b4c542caa3e9b4a1ee83442222ccafc60ce061
Lucas Schaff <90869847+lmschaff@users.noreply.github.com>
2023-02-16 15:23:37 -0600

    Squashed commit of the following:
    commit 22b578db3f009dddb1bd6beeb40e16ff50902fc5
    Merge: ad276b9 f838ac0
    Author: Blackkirby72 <47089182+Blackkirby72@users.noreply.github.com>
    Date:   Thu Feb 16 15:22:37 2023 -0600
    
        Merge pull request #21 from PistolRcks/delete-bundle-css
    
        Delete bundle.css
    
    commit f838ac088c9f7946be57fa4a143b1b203350aa38
    Author: Blackkirby72 <47089182+Blackkirby72@users.noreply.github.com>
    Date:   Thu Feb 16 15:20:36 2023 -0600
    
        Delete bundle.css
    
    commit ad276b901f6e7729740218e34336cf2f967ccd25
    Merge: e7401c8 7183c13
    Author: kreklb <98837415+kreklb@users.noreply.github.com>
    Date:   Thu Feb 16 14:12:15 2023 -0600
    
        Merge pull request #20 from PistolRcks/landing-page-ui
    
        Made UI look better
    
    commit 7183c13043a0c33844ae51b86c65e0a00f69dee2
    Merge: e471605 e7401c8
    Author: Ella Prew <prewe5294@my.uwstout.edu>
    Date:   Thu Feb 16 13:53:35 2023 -0600
    
        Merge branch 'master' into landing-page-ui
    
    commit e471605442b5452898d165d410fdc8204d20ddab
    Author: Ella Prew <prewe5294@my.uwstout.edu>
    Date:   Wed Feb 15 15:58:39 2023 -0600
    
        images and icons working
    
    commit ed6c4adf8a672368da819823417b906d5fe1bd8e
    Author: Ella Prew <prewe5294@my.uwstout.edu>
    Date:   Tue Feb 14 17:44:26 2023 -0600
    
        fixed css styling, added header for puzzle selection page
    
    commit 14e7319d55ae1a2f37dcb8adc8929a92851c219c
    Author: Ella Prew <prewe5294@my.uwstout.edu>
    Date:   Tue Feb 14 16:50:54 2023 -0600
    
        styled log in module and puzzle selection page to match new color scheme
    
    commit 1756bb51013fac19142ca59f3f7d7fd3765c1c7a
    Merge: 9873b3a a7ccd3f
    Author: Ella Prew <prewe5294@my.uwstout.edu>
    Date:   Tue Feb 14 16:18:42 2023 -0600
    
        Merge branch 'landing-page-ui' of https://github.com/PistolRcks/web-puzzle-project into landing-page-ui
    
    commit 9873b3a01ea8ce0c48062eeff0f23f6042f9b9fe
    Author: Ella Prew <prewe5294@my.uwstout.edu>
    Date:   Tue Feb 14 16:18:39 2023 -0600
    
        made landing page look better
    
        still needs png and jpg support in order to use my pictures to make it even better
    
    commit a7ccd3fb395690b314ab967bd10e6c22fe4b06f0
    Merge: f7c4d87 107b9da
    Author: kantab <brendan.k29@gmail.com>
    Date:   Tue Feb 14 14:35:01 2023 -0600
    
        Merge branch 'landing-page-ui' of https://github.com/PistolRcks/web-puzzle-project into landing-page-ui
    
    commit f7c4d87a1403e7eb4dd3297d4429f19e3d6e238a
    Author: kantab <brendan.k29@gmail.com>
    Date:   Tue Feb 14 14:34:57 2023 -0600
    
        changed the login form some
    
    commit 107b9da3504b07bd1e3eff5e3ad83753093b14b7
    Merge: a64b134 52e8600
    Author: Ella Prew <prewe5294@my.uwstout.edu>
    Date:   Tue Feb 14 12:58:00 2023 -0600
    
        Merge branch 'master' into landing-page-ui
    
    commit a64b13492ba41bffa616a825bbd50727caa56db0
    Author: Ella Prew <prewe5294@my.uwstout.edu>
    Date:   Tue Feb 14 12:47:09 2023 -0600
    
        minor changes, changed puzzlepage.js to puzzlepage.jsx

--- client/pages/LandingPage/LandingPage.jsx
+++ client/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Container, Form, Modal, Row } from 'react-bootstrap';
+import puzzlePiece from "../../assets/puzzle-piece.png";
 import "./LandingPage.css";
 
 export default function LandingPage() {
@@ -14,24 +15,25 @@ export default function LandingPage() {
     };
 
     return (
-        <div className="App">
-            <h1>Welcome to our Web Puzzle</h1>
-            <Container className="container">
-
-            <h3>This website is made to challenge the user to solve multiple 
-                different puzzles that will require them to learn about the tricks of 
-                websites and the hidden information they carry. Each puzzle will get you 
-                closer to solving the hidden message.
-            </h3>
-            <br />
+        <div className="app min-vh-100 min-vw-100">
+            <Container className="min-vw-100 header-container">
+                <div><img src={puzzlePiece} alt="puzzle piece" width="50" height="70" />  Welcome to our Web Puzzle</div>
             </Container>
-            <Container className="container">
-            <h6>All features of this website are intended, it's not a bug, it's a feature. 
-                Make sure to explore the site in its entirety in order to solve the puzzles.
-            </h6>
-            <br />
+            <Container className="text-container">
+                <h3>This website is made to challenge the user to solve multiple 
+                    different puzzles that will require them to learn about the tricks of 
+                    websites and the hidden information they carry. Each puzzle will get you 
+                    closer to solving the hidden message.
+                </h3>
+                <br />
             </Container>
-            <Button className="buttons" onClick={handleShow}>Log In</Button>
+            <Container className="text-container">
+                <h6>All features of this website are intended; it's not a bug, it's a feature. 
+                    Make sure to explore the site in its entirety in order to solve the puzzles.
+                </h6>
+                <br />
+            </Container>
+            <Button className="button" onClick={handleShow}>Log In</Button>
             <Modal
                 show={show}
                 onHide={handleClose}
@@ -41,41 +43,54 @@ export default function LandingPage() {
                 aria-labelledby="contained-modal-title-vcenter"
                 centered
                 >
-                    <Modal.Header closeButton>
-                        <Modal.Title id="contained-modal-title-vcenter">
-                            Log In
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form onSubmit={handleSubmit}>
-                            <Row className="mb-3">
-                                <Form.Text>Enter Username:</Form.Text>
-                                <Form.Control
-                                    type="text"
-                                    id="username"
-                                    placeholder="Username"
-                                    name="username"
-                                    />
-                            </Row>
-                            <Row className="mb-3">
-                                <Form.Text>Enter Password:</Form.Text>
-                                <Form.Control
-                                    type="text"
-                                    id="password"
-                                    placeholder="Password"
-                                    name="password"
-                                    />
-                            </Row>
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Log In
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={handleSubmit}>
+                    
+                        <div className="mb-3">
+                        <label>User Name</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            placeholder="Enter User Name"
+                        />
+                        </div>
+                        <div className="mb-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Enter password"
+                        />
+                        </div>
+                        <div className="mb-3 custom-control custom-checkbox">
+                            <input
+                            type="checkbox"
+                            className="custom-control-input"
+                            id="customCheck1"
+                            />
+                            <label className="custom-control-label" htmlFor="customCheck1">
+                            Remember me
+                            </label>
+                        </div>
+                        
+                        <p className="create-account">
+                        Need to <a href="#">Create Account?</a>
+                        </p>
 
-                        </Form>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>Close</Button>
-                        <Link to="/PuzzleSelection">
-                        <Button variant="primary" type="submit" onClick={handleClose}>Log In</Button>   
-                         {/* ^ should verify log in information*/}
-                         </Link>
-                    </Modal.Footer>
+                        </Form> 
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button className="close-button" variant="secondary" onClick={handleClose}>Close</Button>
+                    <Link to="/PuzzleSelection">
+                        <Button className="button" variant="primary" type="submit" onClick={handleClose}>Log In</Button>   
+                        {/* ^ should verify log in information*/}
+                        </Link>
+                </Modal.Footer>
             </Modal>
         </div>
     );
