926ce8e91a340996f834e87a5450edb2170ba43c
PistolRcks <17933691+PistolRcks@users.noreply.github.com>
2023-02-16 14:22:46 -0600

    Separate insert user callback into separate function

--- server/api/signup.js
+++ server/api/signup.js
@@ -1,4 +1,3 @@
-const Express = require('express');
 const Crypto = require('crypto');
 
 var db = require('../db');
@@ -14,38 +13,51 @@ var db = require('../db');
  * This was also shamelessly stolen from the passport tutorial.
  */
 function signup(req, res, next) {
-    if (!req.body.password || !req.body.username) {
-      res.status(400).send("Error: Username or password not set!");
-      return next("Error: Username or password not set!");
+  // check that signup data is real
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send("Error: Username or password not set!");
+    return next("Error: Username or password not set!");
+  }
+
+  var salt = Crypto.randomBytes(16);      // salt is required for hashing; it makes 
+                                          // otherwise identical hashes different
+  Crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+    if (err) {
+      res.status(500).send(err); 
+      return next(err); 
     }
 
-    var salt = Crypto.randomBytes(16);      // salt is required for hashing; it makes 
-                                            // otherwise identical hashes different
-    Crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-      if (err) {
-        res.status(500).send(err); 
-        return next(err); 
-      }
-      // insert new user into database
-      db.run('INSERT INTO User (username, hashed_password, salt) VALUES (?, ?, ?)', [
-        req.body.username,
-        hashedPassword,
-        salt
-      ], function(err) {
-        if (err) {
-          // TODO: Send more descriptive errors
-          res.status(500).send(err) 
-          return next(err); 
-        }
-        var user = {
-          id: this.lastID,
-          username: req.body.username
-        };
-        // TODO: After correctly signing up, log the user in
+    // insert new user into database
+    try {
+      var newUser = insertUser(req.body.username, hashedPassword, salt);
+      res.status(200).send(`Successfully signed user ${req.body.username} up!`)
+      // TODO (integration): After correctly signing up, log the user in
+    } catch (err) {
+      res.status(500).send(`Error: Failed to insert new user!\nSpecific error: ${err}`);
+    }
+  });
+};
+ 
 
-        res.status(200).send(`Successfully signed user ${user.username} up!`)
-      });
-    });
-  };
-  
-  module.exports = signup;
+function insertUser(username, hashedPassword, salt) {
+  var user;
+  db.run('INSERT INTO User (username, hashed_password, salt) VALUES (?, ?, ?)', [
+    username,
+    hashedPassword,
+    salt
+  ], function(err) {
+    // throw an error if there's an issue
+    if (err) {
+      throw err; 
+    }
+
+    // return the new user from the callback
+    user = {
+      id: this.lastID,
+      username: username
+    };
+  });
+  return user;
+}
+
+module.exports = signup;
