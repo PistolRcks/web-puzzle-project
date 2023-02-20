313a73e450a5ae4d080d47f26f650673f8d67c8e
Connor McKellips <79383385+connormckellips@users.noreply.github.com>
2023-02-20 13:24:53 -0600

    Prettified login.js

--- server/api/login.js
+++ server/api/login.js
@@ -1,45 +1,54 @@
-const Express = require('express');
-const Crypto = require('crypto');
+const Express = require("express");
+const Crypto = require("crypto");
 
-var db = require('../db');
+var db = require("../db");
 
 function login(req, res, next) {
-    if (!req.body.password || !req.body.username) {
-      res.status(400).send("Error: Username or password not set!");
-      return next("Error: Username or password not set!");
-    }
+  if (!req.body.password || !req.body.username) {
+    res.status(400).send("Error: Username or password not set!");
+    return next("Error: Username or password not set!");
+  }
 
-    // select existing user in database
-    db.all(`SELECT hashed_password, salt FROM User WHERE username = ${req.body.username}`, [
-        hashedPassword,
-        salt
-      ], function(err) {
-        // if entered username isn't found, send error
-        if (err) {
-          // TODO: Eventually place more speciic errors in here
-          res.status(500).send(err) 
-          return next(err); 
+  // select existing user in database
+  db.all(
+    `SELECT hashed_password, salt FROM User WHERE username = ${req.body.username}`,
+    [hashedPassword, salt],
+    function (err) {
+      // if entered username isn't found, send error
+      if (err) {
+        // TODO: Eventually place more speciic errors in here
+        res.status(500).send(err);
+        return next(err);
+      }
+
+      // if entered username is found, encrypt entered password with existing salt
+      Crypto.pbkdf2(
+        req.body.password,
+        salt,
+        310000,
+        32,
+        "sha256",
+        function (err, attemptedPassword) {
+          // if an error was made upon doing this, send an error
+          if (err) {
+            // TODO: send specific errors eventually
+            res.status(500).send(err);
+            return next(err);
+          }
+
+          // if the attempted password matches the existing password, let user know login
+          // was a success, otherwise send an error
+          if (attemptedPassword == hashedPassword) {
+            res
+              .status(200)
+              .send(`Successfully logged ${req.body.username} in!`);
+          } else {
+            res.status(500).send(err);
+          }
         }
+      );
+    }
+  );
+}
 
-        // if entered username is found, encrypt entered password with existing salt
-        Crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, attemptedPassword) {
-            // if an error was made upon doing this, send an error
-            if (err) {
-                // TODO: send specific errors eventually
-                res.status(500).send(err);
-                return next(err);
-            }
-
-            // if the attempted password matches the existing password, let user know login
-            // was a success, otherwise send an error
-            if (attemptedPassword == hashedPassword) {
-                res.status(200).send(`Successfully logged ${req.body.username} in!`);
-            }
-            else {
-                res.status(401).send(err);
-            }
-        });
-      });
-  };
-  
-  module.exports = login;
+module.exports = login;
