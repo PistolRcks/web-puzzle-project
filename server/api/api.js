b88fb3d4c2f2ae719b7f5ba84a36c74ce8b4fb86
Connor McKellips <79383385+connormckellips@users.noreply.github.com>
2023-02-20 13:23:14 -0600

    Prettified api.js

--- server/api/api.js
+++ server/api/api.js
@@ -1,16 +1,17 @@
-const Express = require('express')
-const signup = require(`./signup`)
-const login = require(`./login`)
+const Express = require("express");
+const signup = require(`./signup`);
+const login = require(`./login`);
 
-const router = new Express.Router()
-router.use(Express.urlencoded({ extended: true }))
-router.use(Express.json())
+const router = new Express.Router();
+router.use(Express.urlencoded({ extended: true }));
+router.use(Express.json());
 
-router.get('/testRoute', (req, res) => {
-  res.send('success')
-})
+router.get("/testRoute", (req, res) => {
+  res.send("success");
+});
 
-router.post('/signup', signup)
-router.post('/login', login)
+router.post("/signup", signup);
+router.post("/login", login);
 
-module.exports = router
+module.exports = router;
+
