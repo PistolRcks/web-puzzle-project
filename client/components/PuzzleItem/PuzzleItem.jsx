24e8a2caa99e34d179f4c865548be5a67be1b75d
Blackkirby72 <47089182+Blackkirby72@users.noreply.github.com>
2023-02-20 18:10:07 -0600

    testing :D

--- client/components/PuzzleItem/PuzzleItem.jsx
+++ client/components/PuzzleItem/PuzzleItem.jsx
@@ -1,48 +1,63 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { Button, Col, Form, Modal, Row } from 'react-bootstrap'
-import x from '../../assets/x.png'
-import check from '../../assets/check.png'
-import './PuzzleItem.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Col, Form, Modal, Row } from "react-bootstrap";
+import x from "../../assets/x.png";
+import check from "../../assets/check.png";
+import "./PuzzleItem.css";
 
 export function PuzzleItem({ puzzle }) {
-  const [show, setShow] = useState(false)
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-  const { puzzleID, isCompleted } = puzzle
+  const { puzzleID, isCompleted } = puzzle;
 
-  const puzzleText = <><img src={isCompleted ? check : x} height='22' alt={isCompleted}/> Puzzle {puzzleID}</>
+  const puzzleText = (
+    <>
+      <img src={isCompleted ? check : x} height="22" alt={String(isCompleted)} /> {`Puzzle ${puzzleID}`}
+    </>
+  );
 
   return (
     <>
       <Row className="mb-3">
-          <Form.Group as={Col} controlId={puzzleID}>
-              <Button className="puzzleSelectionPage__button puzzleItem__button" onClick={handleShow} size="lg" style={{minWidth: '100'}}>
+        <Form.Group as={Col} controlId={puzzleID}>
+          <Button
+            className="puzzleSelectionPage__button puzzleItem__button"
+            onClick={handleShow}
+            size="lg"
+            style={{ minWidth: "100" }}
+          >
+            {puzzleText}
+          </Button>
+          <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
                 {puzzleText}
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>This is a modal body</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
               </Button>
-              <Modal 
-                show={show}
-                onHide={handleClose}
-                backdrop='static'
-                keyboard={false}
-                size='lg'
-                aria-labelledby='contained-modal-title-vcenter'
-                centered
-                >
-                  <Modal.Header closeButton>
-                    <Modal.Title id='contained-modal-title-vcenter'>{puzzleText}</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>This is a modal body</Modal.Body>
-                  <Modal.Footer>
-                    <Button variant='secondary' onClick={handleClose}>Close</Button>
-                    <Link to={`/Puzzle/${puzzleID}`}>
-                      <Button variant='primary' type='submit' onClick={handleClose}>Begin</Button>
-                    </Link>
-                  </Modal.Footer>
-              </Modal>
-          </Form.Group>
+              <Link to={`/Puzzle/${puzzleID}`}>
+                <Button variant="primary" type="submit" onClick={handleClose}>
+                  Begin
+                </Button>
+              </Link>
+            </Modal.Footer>
+          </Modal>
+        </Form.Group>
       </Row>
     </>
-  )
+  );
 }
--- client/pages/PuzzleSelectionPage/PuzzleSelectionPage.jsx
+++ client/pages/PuzzleSelectionPage/PuzzleSelectionPage.jsx
@@ -1,70 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button, Form, Container, Row, Col } from 'react-bootstrap'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Form, Container, Row, Col } from "react-bootstrap";
 
-import back from '../../assets/back-arrow.png';
+import back from "../../assets/back-arrow.png";
 
-import { PuzzleItem } from '../../components/PuzzleItem/PuzzleItem.jsx'
-import './PuzzleSelectionPage.css';
+import { PuzzleItem } from "../../components/PuzzleItem/PuzzleItem.jsx";
+import "./PuzzleSelectionPage.css";
 
-export default function PuzzleSelection() {
-  const { userID, userIcon, puzzles } = RetrievePuzzleData()
+export default function PuzzleSelectionPage() {
+  const { userID, userIcon, puzzles } = RetrievePuzzleData();
 
   return (
-      <div className="puzzleSelectionPage min-vh-100 min-vw-100">
-        <div className="puzzleSelectionPage__backButton">
-          <Link to="/">
-            <Button className="puzzleSelectionPage__button" width='150'>
-              <img src={back} alt="back" width="22" height="22"/> Back
-            </Button>
-          </Link>
-        </div>
-        <Container fluid className='puzzleSelectionPage__selection-header'>
-          <p className="puzzleSelectionPage__selection-title">Puzzle Selection</p>
-        </Container>
-        {
-          // TODO: This link will likely not work until we know how we are handling 
-          // TODO: user authentication and managing the userID after logging in
-        }
-        <div className='puzzle_selection_page__profile-link'>
-          <Link to={`/UserProfile/${userID}`}>
-            <img src={userIcon} alt='User profile' height='75'/>
-          </Link>
-        </div>
-        <div>
-          <Form>
-            {puzzles.map(puzzle => {
-              return (<PuzzleItem puzzle={puzzle} />)
-            })}
-          </Form>
-        </div>
+    <div className="puzzleSelectionPage min-vh-100 min-vw-100">
+      <div className="puzzleSelectionPage__backButton">
+        <Link to="/">
+          <Button className="puzzleSelectionPage__button" width="150">
+            <img src={back} alt="back" width="22" height="22" /> Back
+          </Button>
+        </Link>
+      </div>
+      <Container fluid className="puzzleSelectionPage__selection-header">
+        <p className="puzzleSelectionPage__selection-title">Puzzle Selection</p>
+      </Container>
+      {
+        // TODO: This link will likely not work until we know how we are handling
+        // TODO: user authentication and managing the userID after logging in
+      }
+      <div className="puzzle_selection_page__profile-link">
+        <Link to={`/UserProfile/${userID}`}>
+          <img src={userIcon} alt="User profile" height="75" />
+        </Link>
+      </div>
+      <div>
+        <Form>
+          {puzzles.map((puzzle) => {
+            return <PuzzleItem puzzle={puzzle} />;
+          })}
+        </Form>
+      </div>
     </div>
   );
 }
 
-// TODO: Update this function to call the API once it's ready
+// TODO: Update this function to call the API once it's ready + unit test
 // TODO: userID will likely be passed around on the front end to reduce database queries
-const RetrievePuzzleData = () => {
+export const RetrievePuzzleData = () => {
   return {
     userID: 1,
-    userIcon: 'https://api.dicebear.com/5.x/adventurer/svg?seed=Gracie&scale=130&radius=20&backgroundType=solid,gradientLinear&randomizeIds=true&backgroundColor=c0aede,b6e3f4,d1d4f9,ffdfbf,ffd5dc',
+    userIcon:
+      "https://api.dicebear.com/5.x/adventurer/svg?seed=Gracie&scale=130&radius=20&backgroundType=solid,gradientLinear&randomizeIds=true&backgroundColor=c0aede,b6e3f4,d1d4f9,ffdfbf,ffd5dc",
     puzzles: [
       {
         puzzleID: 1,
-        isCompleted: true
+        isCompleted: true,
       },
       {
         puzzleID: 2,
-        isCompleted: false
+        isCompleted: false,
       },
       {
         puzzleID: 3,
-        isCompleted: false
+        isCompleted: false,
       },
       {
         puzzleID: 4,
-        isCompleted: true
-      }
-    ]
-  }
-}
+        isCompleted: true,
+      },
+    ],
+  };
+};
