8b82d4aaae7ae4b72ea3d2f349f3feea4a898ae5
Connor McKellips <79383385+connormckellips@users.noreply.github.com>
2023-02-20 18:41:04 -0600

    Added test file for user login backend

---
+++ tests/server/login.test.js
@@ -0,0 +1,13 @@
+const myApp = require("../../server/index");
+const supertest = require("supertest");
+const request = supertest(myApp);
+
+describe("Tests for login.js", async () => {
+  it("login - Missing Data 400", async () => {
+    const response = await request.post("/api/login").send({
+      username: "missingPassword",
+    });
+
+    expect(response.statusCode).toEqual(400);
+  });
+});
