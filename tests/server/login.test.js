38f0968eb87ec5d67c12406f6a587d93fd4beb24
Connor McKellips <79383385+connormckellips@users.noreply.github.com>
2023-02-20 18:54:31 -0600

    Installed supertest

--- tests/server/login.test.js
+++ tests/server/login.test.js
@@ -1,7 +1,6 @@
 const myApp = require("../../server/index");
 const supertest = require("supertest");
 const request = supertest(myApp);
-
 describe("Tests for login.js", async () => {
   it("login - Missing Data 400", async () => {
     const response = await request.post("/api/login").send({
