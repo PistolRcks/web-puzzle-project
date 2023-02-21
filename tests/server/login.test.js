9f4186de3f5998aafee8ab4c8035d17f95fbb8ca
Connor McKellips <79383385+connormckellips@users.noreply.github.com>
2023-02-20 19:20:48 -0600

    Edited test file

--- tests/server/login.test.js
+++ tests/server/login.test.js
@@ -1,9 +1,9 @@
 const myApp = require('../../server/index');
-const supertest = require('supertest');
-const request = supertest(myApp);
+const Supertest = require('supertest');
+const request = Supertest(myApp);
 
-describe("Tests for login.js", async () => {
-  it("login - Missing Data 400", async () => {
+describe('Tests for login.js', async () => {
+  it('login - Missing Data 400', async () => {
     const response = await request.post("/api/login").send({
       username: "missingPassword",
     });
