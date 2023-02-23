3cc9a9e9708452c40523809f6b73d296564330b0
Connor McKellips <79383385+connormckellips@users.noreply.github.com>
2023-02-23 12:30:21 -0600

    Let's see if this works

--- tests/server/login.test.js
+++ tests/server/login.test.js
@@ -1,9 +1,9 @@
-const myApp = require('../../server/index');
-const Supertest = require('supertest');
-const request = Supertest(myApp);
+const login = require("./login");
+import supertest from "supertest";
+const request = supertest(login);
 
-describe('Tests for login.js', async () => {
-  it('login - Missing Data 400', async () => {
+describe("Tests for login.js", async () => {
+  it("login - Missing Data 400", async () => {
     const response = await request.post("/api/login").send({
       username: "missingPassword",
     });
