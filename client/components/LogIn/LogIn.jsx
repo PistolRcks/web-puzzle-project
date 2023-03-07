d7a31620039449f1c6da751c86ae6d31200d17fb
kantab <31769818+kantab@users.noreply.github.com>
2023-03-07 14:20:29 -0600

    prettier

--- client/components/LogIn/LogIn.jsx
+++ client/components/LogIn/LogIn.jsx
@@ -1,67 +1,73 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 //import { Link } from 'react-router-dom';
-import {Link, Routes, Route, useNavigate} from 'react-router-dom';
-import { Button, Container, Form, Modal, Row } from 'react-bootstrap';
-import PuzzleSelectionPage from '../../pages/PuzzleSelectionPage/PuzzleSelectionPage';
+import { Link, Routes, Route, useNavigate } from "react-router-dom";
+import { Button, Container, Form, Modal, Row } from "react-bootstrap";
+import PuzzleSelectionPage from "../../pages/PuzzleSelectionPage/PuzzleSelectionPage";
 
 //Default values for form data
 const initialFormData = Object.freeze({
-    username: "",
-    password: "",
-    
+  username: "",
+  password: "",
 });
 function LogIn(props) {
-     //formData is an object that holds username, password, confirmPassword
-     const [formData, updateFormData] = React.useState(initialFormData);
-     const navigate = useNavigate();
-     //Whenever username or confirmPassword input boxes change, this saves the new data to formData
-     const handleChange = (e) => {
-         updateFormData({
-             ...formData,
-             [e.target.name]: e.target.value.trim()
-         });
-     }
-     //Whenever the submit button is clicked, this checks to make sure the passwords match and calls another func
-     const handleSubmit = (e) => {
-         e.preventDefault();
-         if(checkPass(formData.password) && checkUsername(formData.username)) {
-             //TODO send the formData to the backend
-             navigate('/Puzzle/Selection');
-             console.log("Hey this code works");
-             props.close();
-         }
-     }
- 
-     return (
-         <Form onSubmit={handleSubmit}>
-             <Row className="mb-3">
-                 <Form.Text>Enter Username:</Form.Text>
-                 <Form.Control
-                     type="text"
-                     data-testid="usernameLogin"
-                     name="username"
-                     onChange={handleChange}
-                     />
-             </Row>
-             <Row className="mb-3">
-                 <Form.Text>Enter Password:</Form.Text>
-                 <Form.Control
-                     type="password"
-                     data-testid="passwordLogin"
-                     name="password"
-                     onChange={handleChange}
-                     />
-             </Row>
-            
-             <Row>
-             <Link to="/Puzzle/Selection">
-             <Button variant="primary" type="submit" onClick={handleSubmit} data-testid="submitButtonLogin">Log In</Button>
-             </Link>
-             </Row>
-         </Form>
-     );
- }
- /*  Checks to make sure the password:
+  //formData is an object that holds username, password, confirmPassword
+  const [formData, updateFormData] = React.useState(initialFormData);
+  const navigate = useNavigate();
+  //Whenever username or confirmPassword input boxes change, this saves the new data to formData
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData,
+      [e.target.name]: e.target.value.trim(),
+    });
+  };
+  //Whenever the submit button is clicked, this checks to make sure the passwords match and calls another func
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (checkPass(formData.password) && checkUsername(formData.username)) {
+      //TODO send the formData to the backend
+      navigate("/Puzzle/Selection");
+      console.log("Hey this code works");
+      props.close();
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row className="mb-3">
+        <Form.Text>Enter Username:</Form.Text>
+        <Form.Control
+          type="text"
+          data-testid="usernameLogin"
+          name="username"
+          onChange={handleChange}
+        />
+      </Row>
+      <Row className="mb-3">
+        <Form.Text>Enter Password:</Form.Text>
+        <Form.Control
+          type="password"
+          data-testid="passwordLogin"
+          name="password"
+          onChange={handleChange}
+        />
+      </Row>
+
+      <Row>
+        <Link to="/Puzzle/Selection">
+          <Button
+            variant="primary"
+            type="submit"
+            onClick={handleSubmit}
+            data-testid="submitButtonLogin"
+          >
+            Log In
+          </Button>
+        </Link>
+      </Row>
+    </Form>
+  );
+}
+/*  Checks to make sure the password:
      has at least 8 characters
      has an uppercase letter
      has a lowercase letter
@@ -70,37 +76,30 @@ function LogIn(props) {
      If these requirments are not met, an alert displays what is missing
      Returns true if these conditions are met
  */
-     const checkPass = (pass) => {
-     let passStr = String(pass);
-     
-     //If the password contains anything else other than letters, numbers, and underscores, this evaluates false
-     if(/^\w+$/.test(passStr)) {
-         return true;
-     }
-     else {
-         alert("The password you entered contains illegal characters")
-         return false;
-     }
- }
- 
- const checkUsername = (user) => {
-     let userStr = String(user);
-     if(userStr.length < 5) {
-         alert("Wrong  Username, Your Username should be atleast 5 characters long")
-         return false;
-     }
-     if(/^\w+$/.test(userStr)) {
-         return true;
-     }
-     else {
-         alert("The username you entered contains illegal characters")
-         return false;
-     }
- }
- 
- export {
-     LogIn,
-     checkPass,
-     checkUsername
- }
+const checkPass = (pass) => {
+  let passStr = String(pass);
 
+  //If the password contains anything else other than letters, numbers, and underscores, this evaluates false
+  if (/^\w+$/.test(passStr)) {
+    return true;
+  } else {
+    alert("The password you entered contains illegal characters");
+    return false;
+  }
+};
+
+const checkUsername = (user) => {
+  let userStr = String(user);
+  if (userStr.length < 5) {
+    alert("Wrong  Username, Your Username should be atleast 5 characters long");
+    return false;
+  }
+  if (/^\w+$/.test(userStr)) {
+    return true;
+  } else {
+    alert("The username you entered contains illegal characters");
+    return false;
+  }
+};
+
+export { LogIn, checkPass, checkUsername };
--- tests/client/LogIn.test.jsx
+++ tests/client/LogIn.test.jsx
@@ -1,62 +1,75 @@
 import {
-    LogIn,
-    checkPass,
-    checkUsername,
-  } from "../../client/components/LogIn/LogIn";
-  import { render, screen } from "@testing-library/react";
-  import userEvent from "@testing-library/user-event";
-  import { BrowserRouter } from "react-router-dom";
-  
-  
-  describe("Tests for Log In", () => {
-    beforeAll(() => {
-      jest.spyOn(console, "log").mockImplementation(() => {});
-      jest.spyOn(console, "error").mockImplementation(() => {});
-    }); 
-    test("Checks for button", () => {
-        const wrapper = render(<BrowserRouter><LogIn /></BrowserRouter>);
-      expect(wrapper.baseElement.outerHTML).toContain("In");
-    });
-    test("Checks for username label", () => {
-        const wrapper = render(<BrowserRouter><LogIn /></BrowserRouter>);
-      expect(wrapper.baseElement.outerHTML).toContain("Username:");
-    });
-    test("Checks for password label", () => {
-      const wrapper = render(<BrowserRouter><LogIn /></BrowserRouter>);
-      expect(wrapper.baseElement.outerHTML).toContain("Password:");
-    });
-    test("checkUsername with accepted username", () => {
-      expect(checkUsername("myUsername")).toBeTruthy();
-    });
-    test("checkUsername with username with special characters", () => {
-      expect(!checkUsername("myUsername$")).toBeTruthy();
-    });
-    test("checkUsername with username that is too short", () => {
-      expect(!checkUsername("user")).toBeTruthy();
-    });
-    test("checkPasswordRequirements with accepted password", () => {
-      expect(checkPass("Password7")).toBeTruthy();
-    });
-    test("checkPasswordRequirements with password with special characters", () => {
-      expect(!checkPass("Password7$")).toBeTruthy();
-    });
-    
-   test("check to make sure typing in the form and submission works", () => {
-      const consoleSpy = jest.spyOn(global.console, "log");
-      const close = jest.fn();
-      const wrapper = render(<BrowserRouter><LogIn close={close} /></BrowserRouter>);
-  
-      const inputUsername = screen.getByTestId("usernameLogin");
-      userEvent.type(inputUsername, "validUsername");
-  
-      const inputPassword = screen.getByTestId("passwordLogin");
-      userEvent.type(inputPassword, "ValidPass7");
-        
-      const submitButton = screen.getByTestId("submitButtonLogin");
-      userEvent.click(submitButton);
-  
-      expect(consoleSpy).toBeCalledWith("Hey this code works");
-    });
+  LogIn,
+  checkPass,
+  checkUsername,
+} from "../../client/components/LogIn/LogIn";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
+
+describe("Tests for Log In", () => {
+  beforeAll(() => {
+    jest.spyOn(console, "log").mockImplementation(() => {});
+    jest.spyOn(console, "error").mockImplementation(() => {});
+  });
+  test("Checks for button", () => {
+    const wrapper = render(
+      <BrowserRouter>
+        <LogIn />
+      </BrowserRouter>
+    );
+    expect(wrapper.baseElement.outerHTML).toContain("In");
+  });
+  test("Checks for username label", () => {
+    const wrapper = render(
+      <BrowserRouter>
+        <LogIn />
+      </BrowserRouter>
+    );
+    expect(wrapper.baseElement.outerHTML).toContain("Username:");
+  });
+  test("Checks for password label", () => {
+    const wrapper = render(
+      <BrowserRouter>
+        <LogIn />
+      </BrowserRouter>
+    );
+    expect(wrapper.baseElement.outerHTML).toContain("Password:");
+  });
+  test("checkUsername with accepted username", () => {
+    expect(checkUsername("myUsername")).toBeTruthy();
+  });
+  test("checkUsername with username with special characters", () => {
+    expect(!checkUsername("myUsername$")).toBeTruthy();
+  });
+  test("checkUsername with username that is too short", () => {
+    expect(!checkUsername("user")).toBeTruthy();
+  });
+  test("checkPasswordRequirements with accepted password", () => {
+    expect(checkPass("Password7")).toBeTruthy();
+  });
+  test("checkPasswordRequirements with password with special characters", () => {
+    expect(!checkPass("Password7$")).toBeTruthy();
   });
 
-  
+  test("check to make sure typing in the form and submission works", () => {
+    const consoleSpy = jest.spyOn(global.console, "log");
+    const close = jest.fn();
+    const wrapper = render(
+      <BrowserRouter>
+        <LogIn close={close} />
+      </BrowserRouter>
+    );
+
+    const inputUsername = screen.getByTestId("usernameLogin");
+    userEvent.type(inputUsername, "validUsername");
+
+    const inputPassword = screen.getByTestId("passwordLogin");
+    userEvent.type(inputPassword, "ValidPass7");
+
+    const submitButton = screen.getByTestId("submitButtonLogin");
+    userEvent.click(submitButton);
+
+    expect(consoleSpy).toBeCalledWith("Hey this code works");
+  });
+});
