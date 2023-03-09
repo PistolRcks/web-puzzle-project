5840205cbd1675231a6d163b1d3d8d296c3fa63c
Maxwell1455 <Lamppam5393@my.uwstout.edu>
2023-03-08 19:37:14 -0600

    HTTPS

--- server/server.js
+++ server/server.js
@@ -3,4 +3,20 @@ dotenv.config()
 
 const app = require('./index.js')
 const PORT = process.env.PORT
-app.listen(PORT, () => console.log(`http://localhost:${PORT}\nListening on port: ${PORT}`))
+// app.listen(PORT, () => console.log(`http://localhost:${PORT}\nListening on port: ${PORT}`))
+//const PORT = process.env.PORT
+// app.listen(PORT, () => console.log(`http://localhost:${PORT}\nListening on port: ${PORT}`))
+
+https
+  .createServer(
+		// Provide the private and public key to the server by reading each
+		// file's content with the readFileSync() method.
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(3000, () => {
+    console.log((`https://localhost:${PORT}\nListening on port: ${PORT}`));
+  });
