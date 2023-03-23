79083e2ad8ccaf43e25080eef6e99bc3b1b76e7f
PistolRcks <17933691+PistolRcks@users.noreply.github.com>
2023-03-23 14:05:39 -0500

    renaming, insert route into API

--- server/api/api.js
+++ server/api/api.js
@@ -3,6 +3,7 @@ const { signup } = require("./signup");
 const { login } = require("./login");
 const { randomWord } = require("./randomWord");
 const { listPuzzles } = require("./listPuzzles");
+const { setUserPuzzleProgress } = require("./setUserPuzzleMeta");
 
 const router = new Express.Router();
 
@@ -21,6 +22,11 @@ router.post("/login", login);
 router.get("/word", randomWord);
 
 router.get("/listPuzzles", listPuzzles);
+
+// UserPuzzle relation metadata update routes
+router.post("/userPuzzle/progress", setUserPuzzleProgress);
+
+// a route for the timer variable might be set here as well
 
 module.exports = router;
 
