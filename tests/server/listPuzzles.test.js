b72784a202157dddd0bfecbc45a94ca895cb257f
Blackkirby72 <cczeck72@gmail.com>
2023-03-23 14:52:13 -0500

    updated test to be much more consistent

--- tests/server/listPuzzles.test.js
+++ tests/server/listPuzzles.test.js
@@ -25,20 +25,18 @@ jest.mock("../../server/middleware", () => {
  * @returns nothing
  */
 function dataHelper(k) {
-  for (let i = 1; i <= k; i++) {
-    db.run(
-      `INSERT INTO Puzzle VALUES (${i}, "test puzzle ${i}", "test description ${i}")`
-    );
-  }
+  db.run(
+    `INSERT INTO Puzzle VALUES (1, "test puzzle 1", "test description 1")`
+  );
 
   db.run(`INSERT INTO User_Puzzle VALUES (1, 1, NULL, 1)`)
 }
 
 // block console logging, generate mock data
 beforeAll(() => {
-  jest.spyOn(console, "log").mockImplementation(() => {});
-  jest.spyOn(console, "error").mockImplementation(() => {});
-  dataHelper(5);
+  jest.spyOn(console, "log").mockImplementation(() => { });
+  jest.spyOn(console, "error").mockImplementation(() => { });
+  dataHelper();
 });
 
 describe("Puzzles endpoint", () => {
@@ -52,31 +50,11 @@ describe("Puzzles endpoint", () => {
         userID: 1,
         username: 'alice',
         puzzles: [
-            {
-              puzzle_id: 1,
-              title: "test puzzle 1",
-              description: "test description 1",
-            },
-            {
-              puzzle_id: 2,
-              title: "test puzzle 2",
-              description: "test description 2",
-            },
-            {
-              puzzle_id: 3,
-              title: "test puzzle 3",
-              description: "test description 3",
-            },
-            {
-              puzzle_id: 4,
-              title: "test puzzle 4",
-              description: "test description 4",
-            },
-            {
-              puzzle_id: 5,
-              title: "test puzzle 5",
-              description: "test description 5",
-            },
+          {
+            puzzle_id: 1,
+            title: "test puzzle 1",
+            description: "test description 1",
+          }
         ],
         userPuzzleCompletion: [
           {
