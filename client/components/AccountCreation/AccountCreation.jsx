ec66f5011b6e0d17d9748aec0260adf624a8cf9e
kreklb <98837415+kreklb@users.noreply.github.com>
2023-03-23 15:04:04 -0500

    Cleaning up validation functions

--- client/components/AccountCreation/AccountCreation.jsx
+++ client/components/AccountCreation/AccountCreation.jsx
@@ -19,16 +19,40 @@ const initialFormData = Object.freeze({
 function AccountCreation(props) {
 
   const [passwordLength, setPasswordLength] = useState(false);
-  const [uppercaseLetter, setUppercaseLetter] = useState(false);
+  const [passwordUpper, setPasswordUpper] = useState(false);
+  const [passwordLower, setPasswordLower] = useState(false);
+  const [passwordNumber, setPasswordNumber] = useState(false);
+  const [passwordSpecial, setPasswordSpecial] = useState(true);
+  const [isPasswordGood, setIsPasswordGood] = useState(false);
+  const [isUsernameGood, setIsUsernameGood] = useState(false);
+  const [isConfirmPasswordGood, setIsConfirmPasswordGood] = useState(false);
+
   //formData is an object that holds username, password, confirmPassword
   const [formData, updateFormData] = React.useState(initialFormData);
   const navigate = useNavigate();
+
+useEffect(() => {
+  const reqs = checkPasswordRequirements(formData.password);
+  setPasswordLength(reqs[0]);
+  setPasswordNumber(reqs[1]);
+  setPasswordUpper(reqs[2]);
+  setPasswordLower(reqs[3]);
+  setPasswordSpecial(reqs[4]);
+  if(!reqs.includes(false)) {
+    setIsPasswordGood(true);
+  }
+}, [formData.password, formData.confirmPassword]);
+
+useEffect(() => {
+  const reqs = checkUsernameRequirements(formData.username);
+  
+}, [formData.username])
 
   //Whenever username or confirmPassword input boxes change, this saves the new data to formData
   const handleChange = (e) => {
     updateFormData({
       ...formData,
-      [e.target.name]: e.target.value.trim(),
+      [e.target.name]: e.target.value.trim()
     });
   };
 
@@ -59,7 +83,7 @@ function AccountCreation(props) {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} className = "AccountCreation">
       <Row className="mb-3">
         <Form.Text>Enter Username:</Form.Text>
         <Form.Control
@@ -94,16 +118,29 @@ function AccountCreation(props) {
           type="submit"
           onClick={handleSubmit}
           data-testid="submitButton"
+          disabled={isPasswordGood && isConfirmPasswordGood && isUsernameGood ? false : true}
         >
           Create Account
         </Button>
       </Row>
       <Row>
         <Form.Text>
-          Password Requirements: <br/>
-          Only letters, numbers, and underscores are allowed <br/>
-          At least 8 characters long: 
-          {passwordLength ? "✓" : "X"}
+          Password Requirements:
+        </Form.Text>
+        <Form.Text className= {passwordSpecial ? "AccountCreation__password-req-false" : "AccountCreation__password-req-true"}>
+        Only letters, numbers, and underscores are allowed
+        </Form.Text>
+        <Form.Text className= {passwordLength ? "AccountCreation__password-req-true" : "AccountCreation__password-req-false"}>
+          At least 8 characters long
+        </Form.Text>
+        <Form.Text className= {passwordUpper ? "AccountCreation__password-req-true" : "AccountCreation__password-req-false"}>
+          At least 1 uppercase letter
+        </Form.Text>
+        <Form.Text className= {passwordLower ? "AccountCreation__password-req-true" : "AccountCreation__password-req-false"}>
+          At least 1 lowercase letter
+        </Form.Text>
+        <Form.Text className= {passwordNumber ? "AccountCreation__password-req-true" : "AccountCreation__password-req-false"}>
+          At least 1 number
         </Form.Text>
       </Row>
     </Form>
--- utilities/AccountValidators.js
+++ utilities/AccountValidators.js
@@ -8,49 +8,26 @@
     Returns true if these conditions are met
 */
 const checkPasswordRequirements = (pass) => {
+  let reqs = [false, false, false, false, true];
   let passStr = String(pass);
-  let length = "X";
-  let uppercase = "X";
-  let lowercase = "X";
-  let number = "X";
-  let count = 0;
 
   if (passStr.length > 7) {
-    length = "✓";
-    count++;
+    reqs[0] = true;
   }
   if (/\d/.test(passStr)) {
-    number = "✓";
-    count++;
+    reqs[1] = true;
   }
   if (/[A-Z]/.test(passStr)) {
-    uppercase = "✓";
-    count++;
+    reqs[2] = true;
   }
   if (/[a-z]/.test(passStr)) {
-    lowercase = "✓";
-    count++;
-  }
-  if (count < 4) {
-    throw Error(
-      "Your password does not meet requirements: \nLength: " +
-        length +
-        "\nUppercase: " +
-        uppercase +
-        "\nLowercase: " +
-        lowercase +
-        "\nDigit: " +
-        number
-    );
+    reqs[3] = true;
   }
   //If the password contains anything else other than letters, numbers, and underscores, this evaluates false
-  if (/^\w+$/.test(passStr)) {
-    return true;
-  } else {
-    throw Error(
-      "Your password contains illegal characters, please make sure it contains letters, numbers, and underscores only"
-    );
+  if ((/^\w+$/.test(passStr))) {
+    reqs[4] = false;
   }
+  return reqs;
 };
 
 const checkUsernameRequirements = (user) => {
