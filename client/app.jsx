1db9dd246d18b65b42c67e3510aa9b4b0dc481c2
Maxwell1455 <Lamppam5393@my.uwstout.edu>
2023-03-22 22:17:19 -0500

    Change view/Start tests

--- client/app.jsx
+++ client/app.jsx
@@ -5,15 +5,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import EntryPoint from './EntryPoint.jsx'
+import EntryPoint from './EntryPoint.jsx';
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-
 // Start point for the entire front end of the web app
+//console.log(process.env.GOOGLE_OAUTH_CLIENTID)
+
 createRoot(
   document.getElementById('root')
   
 ).render(
-  <GoogleOAuthProvider clientId="493401844468-vdnblepq77sjvjgjjsebd6i1ufmjt3l2.apps.googleusercontent.com"> <EntryPoint/></GoogleOAuthProvider>
+  <GoogleOAuthProvider clientId = "493401844468-vdnblepq77sjvjgjjsebd6i1ufmjt3l2.apps.googleusercontent.com"> <EntryPoint/></GoogleOAuthProvider>
 )
