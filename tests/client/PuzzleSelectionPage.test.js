d372e656eacb7baf77c03402aac175af0a4ee908
kantab <31769818+kantab@users.noreply.github.com>
2023-03-28 12:37:53 -0500

    test

--- tests/client/PuzzleSelectionPage.test.js
+++ tests/client/PuzzleSelectionPage.test.js
@@ -33,7 +33,7 @@ describe("Tests for the Puzzle Selection Page", () => {
       puzzleSelectionPage.baseElement.outerHTML.includes("Puzzle 16509")
     ).toBe(false);
   });
-  it("Checks for Puzzle 1 button", () => {
+  it("Checks for Log Out button", () => {
     const puzzleSelectionPage = render(<PuzzleSelectionPage />);
     expect(puzzleSelectionPage.baseElement.outerHTML).toContain("Log Out");
   });
