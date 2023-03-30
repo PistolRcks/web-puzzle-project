4fa4edcdd2abca2fa9436bd50db679dbb95239f2
Connor McKellips <connor@mckellips.com>
2023-03-30 12:36:10 -0500

    Completed Error 404 test
    Placed in Errors.test.js

--- tests/client/Errors.test.js
+++ tests/client/Errors.test.js
@@ -1,4 +1,5 @@
 import { Error401 } from "../../client/components/Errors/Errors";
+import { Error404 } from "../../client/components/Errors/Errors";
 import { fireEvent, render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { createMemoryHistory } from "@remix-run/router";
@@ -7,5 +8,10 @@ describe("Tests for Errors", () => {
   test("Checks for the 401 - Unauthorized header", () => {
     const error401 = render(<BrowserRouter><Error401 /></BrowserRouter>);
     expect(error401.baseElement.outerHTML).toContain("401 - Unauthorized");
+  })
+
+  test("Checks for the 404 - Not found header", () => {
+    const error404 = render(<BrowserRouter><Error404 /></BrowserRouter>);
+    expect(error404.baseElement.outerHTML).toContain("404 - Not Found");
   })
 })
