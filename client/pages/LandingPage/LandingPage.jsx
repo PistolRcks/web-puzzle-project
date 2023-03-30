786eef5447ca3093692272923281564492a9c80e
Ella Prew <prewe5294@my.uwstout.edu>
2023-03-30 13:05:17 -0500

    standardized formatting on all my files
    made tabs 2 spaces and all quotations " instead of ' and removed unused imports

--- client/pages/LandingPage/LandingPage.jsx
+++ client/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-//import { Link } from 'react-router-dom';
 import { Button, Container, Form, Modal } from 'react-bootstrap';
 import puzzlePiece from "../../assets/puzzle-piece.png";
 import "./LandingPage.css";
@@ -7,123 +6,127 @@ import { AccountCreation } from '../../components/AccountCreation/AccountCreatio
 import {LogIn} from '../../components/LogIn/LogIn';
 
 export default function LandingPage() {
-    const [showLogin, setShowLogin] = useState(false);
-    const handleCloseLogin = () => setShowLogin(false);
-    const handleShowLogin = () => setShowLogin(true);
+  const [showLogin, setShowLogin] = useState(false);
+  const handleCloseLogin = () => setShowLogin(false);
+  const handleShowLogin = () => setShowLogin(true);
 
-    const [showCreate, setShowCreate] = useState(false);
-    const handleCloseCreate = () => setShowCreate(false);
-    const handleShowCreate = () => setShowCreate(true);
+  const [showCreate, setShowCreate] = useState(false);
+  const handleCloseCreate = () => setShowCreate(false);
+  const handleShowCreate = () => setShowCreate(true);
 
-    const initialFormData = Object.freeze({
-        username: "",
-        password: "",
-        confirmPassword: ""
-    }); 
+  const initialFormData = Object.freeze({
+    username: "",
+    password: "",
+    confirmPassword: ""
+  }); 
 
-    const [formData, updateFormData] = React.useState(initialFormData);
+  const [formData, updateFormData] = React.useState(initialFormData);
+
   
-    
-    const handleChange = (e) => {
-        updateFormData({
-            ...formData,
-            [e.target.name]: e.target.value.trim()
-        });
-    }
-    
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData,
+      [e.target.name]: e.target.value.trim()
+    });
+  }
+  
 
 
-    return (
-        <div className="app min-vh-100 min-vw-100" data-testid="landing-1">
-            <Container className="min-vw-100 header-container">
-                <div>
-                    <img 
-                    src={puzzlePiece} 
-                    alt="puzzle piece" 
-                    width="50" 
-                    height="50" /> 
-                    Welcome to our Web Puzzle
-                </div>
-            </Container>
-            <Container className="text-container">
-                <h3>This website is made to challenge the user to solve multiple 
-                    different puzzles that will require them to learn about the tricks of 
-                    websites and the hidden information they carry. Each puzzle will get you 
-                    closer to solving the hidden message.
-                </h3>
-                <br />
-            </Container>
-            <Container className="text-container">
-                <h6>All features of this website are intended; it's not a bug, it's a feature. 
-                    Make sure to explore the site in its entirety in order to solve the puzzles.
-                </h6>
-                <br />
-            </Container>
-            <Button 
-                className="button" 
-                variant="secondary" 
-                onClick={handleShowLogin}>
-                Log In
-            </Button>
-            <Modal
-                show={showLogin}
-                onHide={handleCloseLogin}
-                backdrop="static"
-                keyboard={false}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-                >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Log In
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <LogIn close={handleCloseLogin}/>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button 
-                        className="close-button"
-                        variant="secondary" 
-                        onClick={handleCloseLogin}>
-                            Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-            <Button 
-                className="button" 
-                variant="secondary" 
-                onClick={handleShowCreate}>
-                Create Account
-            </Button>
-            <Modal
-                show={showCreate}
-                onHide={handleCloseCreate}
-                backdrop="static"
-                keyboard={false}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-                >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Create Account
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <AccountCreation close={handleCloseCreate}/>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button 
-                        className="close-button" 
-                        variant="secondary" 
-                        onClick={handleCloseCreate}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+  return (
+    <div className="app min-vh-100 min-vw-100" data-testid="landing-1">
+      <Container className="min-vw-100 header-container">
+        <div>
+          <img 
+          src={puzzlePiece} 
+          alt="puzzle piece" 
+          width="50" 
+          height="50" /> 
+          Welcome to our Web Puzzle
         </div>
-      
-    );
+      </Container>
+      <Container className="text-container">
+        <h3>This website is made to challenge the user to solve multiple 
+            different puzzles that will require them to learn about the tricks of 
+            websites and the hidden information they carry. Each puzzle will get you 
+            closer to solving the hidden message.
+        </h3>
+        <br />
+      </Container>
+      <Container className="text-container">
+        <h6>All features of this website are intended; it's not a bug, it's a feature. 
+            Make sure to explore the site in its entirety in order to solve the puzzles.
+        </h6>
+        <br />
+      </Container>
+      <Button 
+        className="button" 
+        variant="secondary" 
+        onClick={handleShowLogin}
+        >
+        Log In
+      </Button>
+      <Modal
+        show={showLogin}
+        onHide={handleCloseLogin}
+        backdrop="static"
+        keyboard={false}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Log In
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <LogIn close={handleCloseLogin}/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button 
+            className="close-button"
+            variant="secondary" 
+            onClick={handleCloseLogin}
+            >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Button 
+        className="button" 
+        variant="secondary" 
+        onClick={handleShowCreate}
+        >
+        Create Account
+      </Button>
+      <Modal
+        show={showCreate}
+        onHide={handleCloseCreate}
+        backdrop="static"
+        keyboard={false}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Create Account
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <AccountCreation close={handleCloseCreate}/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button 
+            className="close-button" 
+            variant="secondary" 
+            onClick={handleCloseCreate}
+            >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+    
+  );
 }
--- client/pages/Puzzle1Page/Puzzle1Page.jsx
+++ client/pages/Puzzle1Page/Puzzle1Page.jsx
@@ -1,187 +1,183 @@
-import React, { useState, useRef } from 'react';
-import { Button, Carousel, Col, Container, Modal, Overlay, Row, Stack, Tooltip } from 'react-bootstrap';
-import { PuzzleHint } from '../../components/PuzzleHint/PuzzleHint';
-import { PuzzleNavBar } from '../../components/PuzzleNavBar/PuzzleNavBar';
+import React, { useState, useRef } from "react";
+import { Button, Carousel, Col, Container, Modal, Overlay, Row, Stack, Tooltip } from "react-bootstrap";
+import { PuzzleHint } from "../../components/PuzzleHint/PuzzleHint";
+import { PuzzleNavBar } from "../../components/PuzzleNavBar/PuzzleNavBar";
 import frog from "../../assets/frog.jpg";
 import cloud from "../../assets/cloud.jpg";
 import clown from "../../assets/clown.jpg";
 import dino from "../../assets/dino.jpg";
 import food from "../../assets/food.jpg";
-import './Puzzle1Page.css';
+import "./Puzzle1Page.css";
 
 export default function Puzzle1Page() {
 
-    const [showOverlay, setShowOverlay] = useState(false);
-    const [showUnicornHint, setShowUnicornHint] = useState(false);
-    const handleShowUnicornHint = () => {
-        setShowUnicornHint(true);
-        console.log("Here's your secret word: Hippo")   //add secret word here? or it can point
-        //a part of the site that can show the secret word, whichever is easier
-    }
-    const handleCloseUnicornHint = () => setShowUnicornHint(false);
-    const target = useRef(null);
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [showTipsHint, setShowTipsHint] = useState(false);
+  const handleShowTipsHint = () => {
+    setShowTipsHint(true);
+    console.log("Here's your secret word: Hippo")   //add secret word here? or it can point
+    //a part of the site that can show the secret word, whichever is easier
+  }
+  const handleCloseTipsHint = () => setShowTipsHint(false);
+  const target = useRef(null);
 
-    return(
-        <>
-            <PuzzleNavBar />
-            <PuzzleHint />
-            <div className="puzzle1 min-vw-100 min-vh-100">
-                <Container className="justify-content-center content">
-                    <Row>
-                        <Stack direction="horizontal" gap={3}>
-                            <div className="page-title">               
-                                Idk what to put here...Cool pictures?
-                            </div>
-                            <div className="ms-auto">
-                                <Button className="button">
-                                    Oogly
-                                </Button>
-                            </div>
-                            <div>
-                                <Button className="button">
-                                    Boogly
-                                </Button>
-                            </div>
-                            <div>
-                                <Button onClick={handleShowUnicornHint} className="button">
-                                    Unicorn
-                                </Button>
-                                <Modal
-                                    show={showUnicornHint}
-                                    onHide={handleCloseUnicornHint}
-                                    backdrop="static"
-                                    size="lg"
-                                    aria-labelledby="contained-modal-title-vcenter"
-                                    centered
-                                    >
-                                    <Modal.Header closeButton>
-                                        <Modal.Title id="contained-modal-title-vcenter">
-                                            Last Hint
-                                        </Modal.Title>
-                                    </Modal.Header>
-                                    <Modal.Body>
-                                        Check the console to complete the puzzle and find your secret word.
-                                    </Modal.Body>
-                                    <Modal.Footer>
-                                        <p>Click the ? in the bottom left for help opening the console</p>
-                                        <Button 
-                                            className="close-button"
-                                            variant="secondary" 
-                                            onClick={handleCloseUnicornHint}>
-                                                Close
-                                        </Button>
-                                    </Modal.Footer>
-                                </Modal>
-                            </div>
-                        </Stack>
-                    </Row>
-                    <Row>
-                        <Container className="main-block">
-                            <Row>
-                                <Col sm={2}>
-                                    <Stack gap={3} className="side-nav">
-                                        <Button className="button">One</Button>
-                                        <Button className="button">Two</Button>
-                                        <Button className="button">Apple</Button>
-                                        <Button className="button">Frog</Button>
-                                        <Button className="button">Bread</Button>
-                                    </Stack>
-                                </Col>
-                                <Col sm={10}>
-                                    <Carousel>
-                                        <Carousel.Item>
-                                            <img
-                                            className="d-block w-100"
-                                            src={cloud}
-                                            alt="First slide"
-                                            />
-                                            <Carousel.Caption className="carousel-captions">
-                                            <h3>Welcome</h3>
-                                            </Carousel.Caption>
-                                        </Carousel.Item>
+  return(
+    <>
+      <PuzzleNavBar />
+      <PuzzleHint />
+      <div className="puzzle1 min-vw-100 min-vh-100">
+        <Container className="justify-content-center content">
+          <Row>
+            <Stack direction="horizontal" gap={3}>
+              <div className="page-title">               
+                How to Photoshop like a Pro
+              </div>
+              <div className="ms-auto">
+                <Button className="button">
+                  Tutorial
+                </Button>
+              </div>
+              <div>
+                <Button className="button">
+                  Software
+                </Button>
+              </div>
+              <div>
+                <Button onClick={handleShowTipsHint} className="button">
+                  Tips
+                </Button>
+                <Modal
+                  show={showTipsHint}
+                  onHide={handleCloseTipsHint}
+                  backdrop="static"
+                  size="lg"
+                  aria-labelledby="contained-modal-title-vcenter"
+                  centered
+                  >
+                  <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                      Last Hint
+                    </Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    Check the console to complete the puzzle and find your secret word.
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <p>Click the ? in the bottom left for help opening the console</p>
+                    <Button 
+                      className="close-button"
+                      variant="secondary" 
+                      onClick={handleCloseTipsHint}
+                      >
+                      Close
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </div>
+            </Stack>
+          </Row>
+          <Row>
+            <Container className="main-block">
+              <Row>
+                <Col sm={2}>
+                  <Stack gap={3} className="side-nav">
+                    <Button className="button">One</Button>
+                    <Button className="button">Two</Button>
+                    <Button className="button">Apple</Button>
+                    <Button className="button">Frog</Button>
+                    <Button className="button">Bread</Button>
+                  </Stack>
+                </Col>
+                <Col sm={10}>
+                  <Carousel>
+                    <Carousel.Item>
+                      <img
+                      className="d-block w-100"
+                      src={cloud}
+                      alt="First slide"
+                      />
+                      <Carousel.Caption className="carousel-captions">
+                        <h3>Welcome</h3>
+                      </Carousel.Caption>
+                    </Carousel.Item>
 
-                                        <Carousel.Item>
-                                            <img
-                                            className="d-block w-100"
-                                            src={dino}
-                                            alt="Second slide"
-                                            />
+                    <Carousel.Item>
+                      <img
+                      className="d-block w-100"
+                      src={dino}
+                      alt="Second slide"
+                      />
 
-                                            <Carousel.Caption className="carousel-captions">
-                                            <h3>Omg dinosaur</h3>
-                                            <p>Scroll to the 5th slide</p>
-                                            </Carousel.Caption>
-                                        </Carousel.Item>
+                      <Carousel.Caption className="carousel-captions">
+                        <h3>Omg dinosaur</h3>
+                        <p>Scroll to the 5th slide</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
 
-                                        <Carousel.Item>
-                                            <img
-                                            className="d-block w-100"
-                                            src={frog}
-                                            alt="Third slide"
-                                            />
-                                            <Carousel.Caption className="carousel-captions">
-                                            <h3>Oooo froggy</h3>
-                                            </Carousel.Caption>
-                                        </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                      className="d-block w-100"
+                      src={frog}
+                      alt="Third slide"
+                      />
+                      <Carousel.Caption className="carousel-captions">
+                        <h3>Oooo froggy</h3>
+                      </Carousel.Caption>
+                    </Carousel.Item>
 
-                                        <Carousel.Item>
-                                            <img
-                                            className="d-block w-100"
-                                            src={food}
-                                            alt="Fourth slide"
-                                            />
-                                            <Carousel.Caption className="carousel-captions">
-                                            <h3>I want melon</h3>
-                                            </Carousel.Caption>
-                                        </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                      className="d-block w-100"
+                      src={food}
+                      alt="Fourth slide"
+                      />
+                      <Carousel.Caption className="carousel-captions">
+                        <h3>I want melon</h3>
+                      </Carousel.Caption>
+                    </Carousel.Item>
 
-                                        <Carousel.Item>
-                                            <img
-                                            className="d-block w-100"
-                                            src={clown}
-                                            alt="Fifth slide"
-                                            />
-                                            <Carousel.Caption className="carousel-captions">
-                                            <h3>Welcome to the fifth slide</h3>
-                                            <p>
-                                                Check the bottom right of the screen
-                                            </p>
-                                            </Carousel.Caption>
-                                        </Carousel.Item>
-                                    </Carousel>
-                                </Col>
-                            </Row>
-                            <Row className="blank-space"></Row>
-                            <Row>
-                                <Col sm={10}></Col>
-                                <Col sm={2}>
-                                <Button 
-                                    ref={target} 
-                                    onClick={() => setShowOverlay(!showOverlay)} 
-                                    className="button first-hint"
-                                    >
-                                    Click me
-                                </Button>
-                                <Overlay 
-                                    target={target.current} 
-                                    show={showOverlay} 
-                                    placement="left"
-                                    >
-                                    {(props) => (
-                                        <Tooltip id="overlay-hint" {...props}>
-                                            Click on the word Unicorn
-                                        </Tooltip>
-                                    )}
-                                </Overlay>
-                                </Col>
-                            </Row>
-                        </Container>                         
-                    </Row>
-
-
-
-                </Container>
-            </div>
-        </>
-    );
+                    <Carousel.Item>
+                      <img
+                      className="d-block w-100"
+                      src={clown}
+                      alt="Fifth slide"
+                      />
+                      <Carousel.Caption className="carousel-captions">
+                        <h3>Welcome to the fifth slide</h3>
+                        <p>Check the bottom right of the screen</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                  </Carousel>
+                </Col>
+              </Row>
+              <Row className="blank-space"></Row>
+              <Row>
+                <Col sm={10}></Col>
+                <Col sm={2}>
+                  <Button 
+                    ref={target} 
+                    onClick={() => setShowOverlay(!showOverlay)} 
+                    className="button first-hint"
+                    >
+                    Click me
+                  </Button>
+                  <Overlay 
+                    target={target.current} 
+                    show={showOverlay} 
+                    placement="left"
+                    >
+                    {(props) => (
+                      <Tooltip id="overlay-hint" {...props}>
+                        Click on the word Unicorn
+                      </Tooltip>
+                    )}
+                  </Overlay>
+                </Col>
+              </Row>
+            </Container>                         
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
 }
