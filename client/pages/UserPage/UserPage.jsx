4f8f4f56dd5e4119bcd18f03fab405cd450348a2
Connor McKellips <connor@mckellips.com>
2023-04-07 14:17:01 -0500

    Added in some more stuff
    More description, but my styling is not the greatest at the moment

--- client/pages/UserPage/UserPage.jsx
+++ client/pages/UserPage/UserPage.jsx
@@ -4,11 +4,13 @@ export default function UserPage () {
     const userIcon =
     "https://api.dicebear.com/5.x/adventurer/svg?seed=Gracie&scale=130&radius=20&backgroundType=solid,gradientLinear&randomizeIds=true&backgroundColor=c0aede,b6e3f4,d1d4f9,ffdfbf,ffd5dc";
 
-
     return(
         <div className="UserPage">
+            <div >
+                <img className = "userPage__ProfilePicture" src = {userIcon} height="75" />
+            </div>
             <div>
-                <img src = {userIcon} height="75" />
+                Profile Page
             </div>
         </div>
     )
