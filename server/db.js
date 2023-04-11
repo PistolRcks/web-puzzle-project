bd330b6c1d59282969e1c2e5869452381e251e34
Blackkirby72 <cczeck72@gmail.com>
2023-04-11 14:14:06 -0500

    made it cool

--- server/db.js
+++ server/db.js
@@ -11,37 +11,51 @@ function initDatabase(fp) {
 
   db.serialize(() => {
     // create the database schema for the app
-    db.run(
-      `CREATE TABLE IF NOT EXISTS User (
+    db.run(`
+      CREATE TABLE IF NOT EXISTS User (
         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE,
         hashed_password BLOB,
-        salt BLOB,
-        profile_picture BLOB,
-        profile_picture_top INTEGER,
-        profile_picture_left INTEGER
-      )`
-    );
+        salt BLOB
+      )
+    `);
 
-    db.run(
-      `CREATE TABLE IF NOT EXISTS Puzzle (
+    db.run(`
+      CREATE TABLE IF NOT EXISTS Puzzle (
         puzzle_id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
         description TEXT
-      )`
-    );
+      )
+    `);
 
-    db.run(
-      `CREATE TABLE IF NOT EXISTS User_Puzzle (
+    db.run(`
+      CREATE TABLE IF NOT EXISTS User_Puzzle (
         user_id INTEGER,
         puzzle_id INTEGER,
         time	INTEGER,
         progress	NUMERIC NOT NULL COLLATE BINARY,
         FOREIGN KEY(user_id) REFERENCES User(user_id),
         FOREIGN KEY(puzzle_id) REFERENCES Puzzle(puzzle_id)
-      )`
-    );
+      )
+    `);
   });
+
+  const columnsToAdd = {
+    User: [
+      { name: "profile_picture", typeDef: "BLOB"},
+      { name: "profile_picture_top", typeDef: "INTEGER"},
+      { name: "profile_picture_left", typeDef: "INTEGER"}
+    ]
+  }
+
+  for (const table in columnsToAdd) {
+    columnsToAdd[table].forEach(column => {
+        db.run(`
+          ALTER TABLE ${table}
+          ADD COLUMN ${column.name} ${column.typeDef}
+        `, (err) => { });
+    });
+  }
 
   return db;
 }
