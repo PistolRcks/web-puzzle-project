d0bced20bf770c60ef2118c67e4e8dec829f48e7
Blackkirby72 <cczeck72@gmail.com>
2023-04-11 13:36:32 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -9,34 +9,37 @@ const Sqlite3 = require("sqlite3");
 function initDatabase(fp) {
   var db = new Sqlite3.Database(fp);
 
-  db.serialize(function () {
+  db.serialize(() => {
     // create the database schema for the app
     db.run(
-      "CREATE TABLE IF NOT EXISTS User ( \
-      user_id INTEGER PRIMARY KEY AUTOINCREMENT, \
-      username TEXT UNIQUE, \
-      hashed_password BLOB, \
-      salt BLOB \
-    )"
+      `CREATE TABLE IF NOT EXISTS User (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        hashed_password BLOB,
+        salt BLOB,
+        profile_picture BLOB,
+        profile_picture_top INTEGER,
+        profile_picture_left INTEGER
+      )`
     );
 
     db.run(
-      "CREATE TABLE IF NOT EXISTS Puzzle ( \
-      puzzle_id INTEGER PRIMARY KEY AUTOINCREMENT, \
-      title TEXT NOT NULL, \
-      description TEXT \
-    )"
+      `CREATE TABLE IF NOT EXISTS Puzzle (
+        puzzle_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT
+      )`
     );
 
     db.run(
-      "CREATE TABLE IF NOT EXISTS User_Puzzle ( \
-      user_id INTEGER, \
-      puzzle_id INTEGER, \
-      time	INTEGER, \
-      progress	NUMERIC NOT NULL COLLATE BINARY, \
-      FOREIGN KEY(user_id) REFERENCES User(user_id), \
-      FOREIGN KEY(puzzle_id) REFERENCES Puzzle(puzzle_id) \
-    )"
+      `CREATE TABLE IF NOT EXISTS User_Puzzle (
+        user_id INTEGER,
+        puzzle_id INTEGER,
+        time	INTEGER,
+        progress	NUMERIC NOT NULL COLLATE BINARY,
+        FOREIGN KEY(user_id) REFERENCES User(user_id),
+        FOREIGN KEY(puzzle_id) REFERENCES Puzzle(puzzle_id)
+      )`
     );
   });
 
