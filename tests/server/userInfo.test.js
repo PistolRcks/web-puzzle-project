f71a740d54df3cda254ce9dab15142747c5eba8e
PistolRcks <17933691+PistolRcks@users.noreply.github.com>
2023-04-20 14:40:25 -0500

    Prettification

--- tests/server/userInfo.test.js
+++ tests/server/userInfo.test.js
@@ -261,150 +261,167 @@ describe("Tests for userInfo.js: setUserPassword", () => {
 });
 
 describe("Tests for userInfo.js: setUserPFP", () => {
-  const route = "/api/user/picture"
+  const route = "/api/user/picture";
   const inputData = {
     profile_picture: "Blob data...",
     profile_picture_top: 0,
-    profile_picture_left: 0
-  }
-  const reqError = "Error: 'profile_picture', 'profile_picture_top', or 'profile_picture_left' not found in the request body!"
-  const typeError = "Error: 'profile_picture_top' or 'profile_picture_left' is not a number!"
-  const boundsError = "Error: 'profile_picture_top' or 'profile_picture_left' is negative!"
+    profile_picture_left: 0,
+  };
+  const reqError =
+    "Error: 'profile_picture', 'profile_picture_top', or 'profile_picture_left' not found in the request body!";
+  const typeError =
+    "Error: 'profile_picture_top' or 'profile_picture_left' is not a number!";
+  const boundsError =
+    "Error: 'profile_picture_top' or 'profile_picture_left' is negative!";
 
   afterEach(() => {
     db.run.mockReset();
   });
-  
+
   test("200 - Normal operation", async () => {
     db.run = jest.fn((query, params, callback) => {
-      callback(null)
-    })
-    
-    const response = await request
-      .post(route)
-      .send(inputData);
-    
-    // Expect parameters to be what they should be
-    expect(db.run.mock.lastCall[1]).toEqual(["Blob data...", 0, 0, 1])
+      callback(null);
+    });
 
-    expect(response.statusCode).toBe(200)
-    expect(response.text).toEqual("Profile picture updated successfully.")
-  })
-  
+    const response = await request.post(route).send(inputData);
+
+    // Expect parameters to be what they should be
+    expect(db.run.mock.lastCall[1]).toEqual(["Blob data...", 0, 0, 1]);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toEqual("Profile picture updated successfully.");
+  });
+
   // the following boilerplate tests brought to you by ChatGPT :)))
   // it's like regex on steroids
   test("400 - profile_picture missing from request body", async () => {
-    const { profile_picture, ...reducedInput} = inputData
-    const response = await request.post(route).send(reducedInput)
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(reqError)
-  })
-  
+    const { profile_picture, ...reducedInput } = inputData;
+    const response = await request.post(route).send(reducedInput);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(reqError);
+  });
+
   test("400 - profile_picture_top missing from request body", async () => {
-    const { profile_picture_top, ...reducedInput} = inputData
-    const response = await request.post(route).send(reducedInput)
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(reqError)
-  })
-  
+    const { profile_picture_top, ...reducedInput } = inputData;
+    const response = await request.post(route).send(reducedInput);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(reqError);
+  });
+
   test("400 - profile_picture_left missing from request body", async () => {
-    const { profile_picture_left, ...reducedInput} = inputData
-    const response = await request.post(route).send(reducedInput)
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(reqError)
-  }) 
-  
+    const { profile_picture_left, ...reducedInput } = inputData;
+    const response = await request.post(route).send(reducedInput);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(reqError);
+  });
+
   test("400 - profile_picture and profile_picture_top missing from request body", async () => {
-    const { profile_picture, profile_picture_top, ...reducedInput} = inputData
-    const response = await request.post(route).send(reducedInput)
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(reqError)
-  })
-  
+    const { profile_picture, profile_picture_top, ...reducedInput } = inputData;
+    const response = await request.post(route).send(reducedInput);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(reqError);
+  });
+
   test("400 - profile_picture_top and profile_picture_left missing from request body", async () => {
-    const { profile_picture_top, profile_picture_left, ...reducedInput} = inputData
-    const response = await request.post(route).send(reducedInput)
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(reqError)
-  })
-  
+    const { profile_picture_top, profile_picture_left, ...reducedInput } =
+      inputData;
+    const response = await request.post(route).send(reducedInput);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(reqError);
+  });
+
   test("400 - profile_picture and profile_picture_left missing from request body", async () => {
-    const { profile_picture, profile_picture_left, ...reducedInput} = inputData
-    const response = await request.post(route).send(reducedInput)
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(reqError)
-  })
+    const { profile_picture, profile_picture_left, ...reducedInput } =
+      inputData;
+    const response = await request.post(route).send(reducedInput);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(reqError);
+  });
 
   test("400 - everything missing from request body", async () => {
-    const response = await request.post(route).send({})
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(reqError)
-  })
+    const response = await request.post(route).send({});
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(reqError);
+  });
 
   test("400 - profile_picture_top is not a number", async () => {
-    const invalidInputData = { ...inputData, profile_picture_top: "invalid_top" }
-    const response = await request.post(route).send(invalidInputData)
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(typeError)
-  })
-  
+    const invalidInputData = {
+      ...inputData,
+      profile_picture_top: "invalid_top",
+    };
+    const response = await request.post(route).send(invalidInputData);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(typeError);
+  });
+
   test("400 - profile_picture_left is not a number", async () => {
-    const invalidInputData = { ...inputData, profile_picture_left: "invalid_left" }
-    const response = await request.post(route).send(invalidInputData)
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(typeError)
-  })
-  
+    const invalidInputData = {
+      ...inputData,
+      profile_picture_left: "invalid_left",
+    };
+    const response = await request.post(route).send(invalidInputData);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(typeError);
+  });
+
   test("400 - both expected numbers are not numbers", async () => {
-    const invalidInputData = { ...inputData, profile_picture_top: "invalid_top", profile_picture_left: "invalid_left" }
-    const response = await request.post(route).send(invalidInputData)
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(typeError)
-  })
- 
+    const invalidInputData = {
+      ...inputData,
+      profile_picture_top: "invalid_top",
+      profile_picture_left: "invalid_left",
+    };
+    const response = await request.post(route).send(invalidInputData);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(typeError);
+  });
 
   test("400 - profile_picture_top is negative", async () => {
-    const invalidInputData = { ...inputData, profile_picture_top: -1 }
-    const response = await request.post(route).send(invalidInputData)
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(boundsError)
-  })
-  
-  test("400 - profile_picture_left is negative", async () => {
-    const invalidInputData = { ...inputData, profile_picture_left: -1 }
-    const response = await request.post(route).send(invalidInputData)
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(boundsError)
-  })
-  
-  test("400 - both numbers are negative", async () => {
-    const invalidInputData = { ...inputData, profile_picture_top: -1, profile_picture_left: -1 }
-    const response = await request.post(route).send(invalidInputData)
-  
-    expect(response.statusCode).toBe(400)
-    expect(response.text).toEqual(boundsError)
-  })
+    const invalidInputData = { ...inputData, profile_picture_top: -1 };
+    const response = await request.post(route).send(invalidInputData);
 
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(boundsError);
+  });
+
+  test("400 - profile_picture_left is negative", async () => {
+    const invalidInputData = { ...inputData, profile_picture_left: -1 };
+    const response = await request.post(route).send(invalidInputData);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(boundsError);
+  });
+
+  test("400 - both numbers are negative", async () => {
+    const invalidInputData = {
+      ...inputData,
+      profile_picture_top: -1,
+      profile_picture_left: -1,
+    };
+    const response = await request.post(route).send(invalidInputData);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toEqual(boundsError);
+  });
+
+  // I actually wrote this one lol
   test("500 - `db.get` error", async () => {
     db.run = jest.fn((query, params, callback) => {
-      callback("Error!")      
-    })
-    
-    const response = await request.post(route).send(inputData)
-    
-    expect(response.statusCode).toBe(500)
-    expect(response.text).toEqual("Error!")
-  })
-})
+      callback("Error!");
+    });
+
+    const response = await request.post(route).send(inputData);
+
+    expect(response.statusCode).toBe(500);
+    expect(response.text).toEqual("Error!");
+  });
+});
